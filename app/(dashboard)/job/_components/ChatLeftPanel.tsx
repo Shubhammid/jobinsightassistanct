20c017b1f637c5b5f19e464a31a60f1556e46336
Shubham Midgule <shubham.midgule17@gmail.com>
2025-10-05 21:56:00 +0530

    Completed Chat UI and API

--- app/(dashboard)/job/_components/ChatLeftPanel.tsx
+++ app/(dashboard)/job/_components/ChatLeftPanel.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 import React from "react";
+import { useUser } from "@clerk/nextjs";
+import ChatComponent from "@/components/ChatComponent";
 
 const ChatLeftPanel = (props: { jobId: string }) => {
+  const { jobId } = props;
+  const { user } = useUser();
   const { open, isMobile } = useSidebar();
+
+  const userId = user?.id || null;
+  const userName = user?.firstName || null;
+
   return (
     <div className="w-full h-screen flex flex-col">
       <div className="h-10 w-full border-b border-gray-200 pt-1 flex items-center px-2">
@@ -11,6 +19,15 @@ const ChatLeftPanel = (props: { jobId: string }) => {
           {(!open || isMobile) && <SidebarTrigger />}
           <h1 className="font-semibold pt-1">Job Insight Mode</h1>
         </div>
+      </div>
+      <div className="flex-1 overflow-hidden">
+        <ChatComponent
+          {...{
+            jobId,
+            userId,
+            userName,
+          }}
+        />
       </div>
     </div>
   );
